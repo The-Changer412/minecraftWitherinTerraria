de2bf7e901ad93857605903b5ba6bcc77866fd85
The-Changer412 <thechanger412@gmail.com>
2021-12-28 20:22:28 -0600

    generated the skeleton

---
+++ MinecraftWither.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace MinecraftWither
+{
+	public class MinecraftWither : Mod
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Minecraft Wither In Terraria
+This is a Terraria mod that adds in the minecraft wither into terraria
