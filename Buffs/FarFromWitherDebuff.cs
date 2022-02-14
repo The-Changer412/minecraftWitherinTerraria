6c5daf1ed31ed12974d34384561c3ab041df893d
The-Changer412 <thechanger412@gmail.com>
2022-02-14 13:12:29 -0600

    renamed the far from wither debuff

--- Buffs/FarFromWitherDebuff.cs
+++ Buffs/FarFromWitherDebuff.cs
@@ -8,7 +8,7 @@ namespace minecraftWitherinTerraria.Buffs
     //set the properties of the wither debuff
     public override void SetDefaults()
     {
-		DisplayName.SetDefault("Wither Armor");
+		DisplayName.SetDefault("Far From Wither Debuff");
 		Description.SetDefault("You are too far away from the Wither to do damage to him.");
 		Main.debuff[Type] = true;
         canBeCleared = false;
