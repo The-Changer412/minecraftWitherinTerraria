92c246f73d014d3c36b91d49af40fe45f21b3ad4
The Changer <thechanger412@gmail.com>
2022-10-14 10:26:52 -0500

    fix a crash

--- MinecraftWither.cs
+++ MinecraftWither.cs
@@ -7,11 +7,14 @@ namespace minecraftWitherinTerraria
 	{
 		public override void PostSetupContent()
 		{
-		    Mod bossChecklist = ModLoader.GetMod("BossChecklist");
-		    if(bossChecklist != null)
-		    {
-				bossChecklist.Call("AddBoss", 15f, ModContent.NPCType<NPCs.Wither>(), this, "Wither", (Func<bool>)(() => World.DownedWither), null, null, ModContent.ItemType<Items.NetherStar>(), "Spawn in the Wither by placing soul sand in a T-pose shape with three Wither skulls on it.", "The Wither has left this world.");
-		    }
+			try
+			{
+                Mod bossChecklist = ModLoader.GetMod("BossChecklist");
+                if (bossChecklist != null)
+                {
+                    bossChecklist.Call("AddBoss", 15f, ModContent.NPCType<NPCs.Wither>(), this, "Wither", (Func<bool>)(() => World.DownedWither), null, null, ModContent.ItemType<Items.NetherStar>(), "Spawn in the Wither by placing soul sand in a T-pose shape with three Wither skulls on it.", "The Wither has left this world.");
+                }
+            } catch {}
 		}
 	}
 }
